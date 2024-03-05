4af17328632eb645d249a98d89082ab155219a19
Stephenmuhindi <bteneson42@gmail.com>
2024-03-05 11:25:25 -0800

    aih

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,27 +4,20 @@ number in subreddit
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
-    Fetches the number of subscribers for a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit.
-        
-    Returns:
-        int: The number of subscribers. Returns 0 if the subreddit does not exist or an error occurs.
+    Function of the above
     """
-    url = f'https://www.reddit.com/r/{subreddit}/about/.json'
-    headers = {
+    url = 'https://www.reddit.com'
+    header = {
         'Accept': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     }
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
-        return response.json().get('data', {}).get('subscribers', 0)
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return 0
-
+    response = requests.get('{}/r/{}/about/.json'.format(url, subreddit),
+                            headers=header,
+                            allow_redirects=False
+                            )
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    return 0
