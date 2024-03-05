2c25612cf83c900aee80b4b892b5fb864720800f
Stephenmuhindi <bteneson42@gmail.com>
2024-03-05 11:49:33 -0800

    dont worry

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
-"""
-number in subreddit
-"""
-
-import requests
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
     """
-    Function to retrieve the number of subscribers for a given subreddit.
-    Returns int: The number of subscribers for the specified subreddit.
+    No authentication is necessary for
     """
-    url = 'https://www.reddit.com'
-    header = {
-        'Accept': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-    }
+    import requests
 
-    """Make the request to fetch subreddit information"""
-    response = requests.get('{}/r/{}/about/.json'.format(url, subreddit),
-                            headers=header,
-                            allow_redirects=False
-                            )
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
 
-    """Check if the response is successful (status code 200)"""
-    if response.status_code == 200:
-        return response.json()['data']['subscribers']
-    """
-    Return 0 if the subreddit does not exist or an error occurs
-    """
-    return 0
+    return sub_info.json().get("data").get("subscribers")
